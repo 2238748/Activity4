5bd0ae596f4862672cbb157e6944d7b8480b2e11
rezielanne <rezielanne19@gmail.com>
2023-11-08 11:15:29 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -8,3 +8,16 @@ address VARCHAR(100) NOT NULL,
 phone VARCHAR(15) NOT NULL,
 email VARCHAR(100) NOT NULL
 )
+
+
+CREATE TABLE animals (
+    animalid INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    species VARCHAR(50) NOT NULL,
+    breed VARCHAR(50) NOT NULL,
+    dateofbirth DATE NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    color VARCHAR(50) NOT NULL,
+    ownerid INT NOT NULL, -- Define data type and NOT NULL constraint
+    FOREIGN KEY (ownerid) REFERENCES owners(ownerid)
+);
